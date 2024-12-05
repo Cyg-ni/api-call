825c4a227772e96a5e98515057a95dd7cef9d44d
Cyg-ni <christiiinereyes@gmail.com>
2024-12-05 18:10:36 +0800

    Finalized Script

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
 const apiKey = '0e5ad6693d134fb1a4f1b82ac1cdbb58aac45d90244847af9d9bf643e4bd9f7e';
-const apiUrl = 'https://xivapi.com/';
-async function getText() {
-    let x = await fetch(apiUrl);
+const tableBody = document.getElementById(
+  'class-actions'
+)
+const currentPage= document.getElementById(
+  'page-number'
+)
+let nextPagetracker = 0;
+let prevPagetracker = 0;
+
+
+
+async function getText(pageNumber) {
+    const apiUrl = `https://xivapi.com/action?private_key=${apiKey}&page=${pageNumber}&limit=15`;
+
+
+    let x = await fetch(apiUrl, {
+      mode:'cors'
+    });
     let y = await x.json();
-    console.log(x);
+    nextPagetracker = y.Pagination.PageNext;
+    prevPagetracker = y.Pagination.PagePrev;
+    console.log (y);
+    let template=``;
+    for (let item of y.Results){
+      template +=  `<tr class="">
+      <td class="font-bold text-red-900 ">${item.Name}</td>
+      <td class="bg-[#fffceb] "><img class="mx-auto size-10 rounded-md" src="https://xivapi.com/${item.Icon}"></td>
+      <td class="bg-[#fffceb] text-red-900 font-bold rounded-md">${item.ID}</td> 
+   </tr>`    
+    }
+    tableBody.innerHTML = template;
+    currentPage.innerText = y.Pagination.Page;
   }
+
+
+function nextPage(){
+  if (nextPagetracker) getText(nextPagetracker)
+}
+
+function prevPage(){
+  if (prevPagetracker) getText(prevPagetracker)
+  
+}
+
+getText();
+  
